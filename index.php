a1c2c50cd624cadfa30f253496c13f5eff8ac5f5
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-06-08 12:58:39 -0300

    add - tentando estilizar tela de login

--- index.php
+++ index.php
@@ -27,29 +27,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
-<body>
-    <h1>Sitema de Login Simples</h1>
+<body class="body1">
 
-    <form method="POST">
-        <label>Usuário:</label>
-        <input type="text" name="usuario">
-        <br>
-        <label>Senha:</label>
-        <input type="password" name="senha">
-        <br>
-        <?php
-        
-            if(isset($erro)){
-                echo $erro;
-            };
-
-            // esse erro serve ara alguma coisa
-        
-        ?>
-        <br>
-        <button type="submit">Entrar</button>
-    </form>
-
+    <main class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-4">
+                <div class="card p-4 shadow-lg">
+                    <div class="position-relative mb-4">
+                        <h1 style="font-size: 35px;">Sitema de Login Simples</h1>
+                        <form method="POST">
+                            <label class="form-label" style="font-size: 18px;">Usuário:</label>
+                            <input type="text" name="usuario">
+                            <br>
+                            <label class="form-label" style="font-size: 18px;">Senha:</label>
+                            <input type="password" name="senha">
+                            <br>
+                            <?php
+                            
+                                if(isset($erro)){
+                                    echo $erro;
+                                };
+                        
+                                // esse erro serve ara alguma coisa
+                            
+                            ?>
+                            <br>
+                            <button class="btn btn-primary w-100 shadow" type="submit">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    
 </body>
 </html>
